50a1533a87ef848cf8a92d284c4f63d1e03ab64e
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-12-20 21:57:58 +0000

    Add basic number functionality

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const calculator = document.querySelector(".calculator");
 const buttons = document.querySelector(".buttons");
-const button_list = []; 
+const calculator_output = document.querySelector(".calculator_output")
+const button_list = [];
+let output_buffer = "";
 for(const child of buttons.children){
+    child.onclick = () => add_number(Number(child.innerHTML));
     button_list.push(child);
+}
+
+function add_number(number){
+    if(output_buffer === ""){
+        output_buffer = number;
+    }
+    else{
+        output_buffer = output_buffer * 10 + number;
+    }
+    calculator_output.innerHTML = output_buffer;
 }
 console.log(button_list);

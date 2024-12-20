b0b66628b7fe217a4b8ba42a714869b1eb078951
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-12-20 22:13:32 +0000

    Add clear all and last functionality

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const calculator_output = document.querySelector(".calculator_output")
 const button_list = [];
 let output_buffer = "";
 for(const child of buttons.children){
-    child.onclick = () => add_number(Number(child.innerHTML));
-    button_list.push(child);
+    if(child.id != "ac" && child.id != "c"){
+        child.onclick = () => add_number(Number(child.innerHTML));
+        button_list.push(child);
+    }
 }
+
+const clearall_button = document.querySelector("#ac");
+clearall_button.onclick = clear_all;
+
+const clearlast_button = document.querySelector("#c");
+clearlast_button.onclick = clear_last;
 
 function add_number(number){
     if(output_buffer === ""){
@@ -14,6 +22,23 @@ function add_number(number){
     }
     else{
         output_buffer = output_buffer * 10 + number;
+    }
+    calculator_output.innerHTML = output_buffer;
+}
+
+function clear_all(){
+    if(output_buffer != ""){
+        output_buffer = "";
+    }
+    calculator_output.innerHTML = output_buffer;
+}
+
+function clear_last(){
+    if(output_buffer === "" || output_buffer < 10){
+        output_buffer = "";
+    }
+    else{
+        output_buffer = (output_buffer - (output_buffer % 10)) / 10
     }
     calculator_output.innerHTML = output_buffer;
 }

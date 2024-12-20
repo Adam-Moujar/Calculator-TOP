25056565541dcee39e265a1b0ad233cc35e661f6
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-12-20 22:21:38 +0000

    Made code simpler

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 const calculator = document.querySelector(".calculator");
 const buttons = document.querySelector(".buttons");
 const calculator_output = document.querySelector(".calculator_output")
-const button_list = [];
 let output_buffer = "";
-for(const child of buttons.children){
-    if(child.id != "ac" && child.id != "c"){
-        child.onclick = () => add_number(Number(child.innerHTML));
-        button_list.push(child);
-    }
-}
-
-const clearall_button = document.querySelector("#ac");
-clearall_button.onclick = clear_all;
-
-const clearlast_button = document.querySelector("#c");
-clearlast_button.onclick = clear_last;
 
 function add_number(number){
     if(output_buffer === ""){
@@ -42,4 +29,19 @@ function clear_last(){
     }
     calculator_output.innerHTML = output_buffer;
 }
-console.log(button_list);
+
+function init_buttons(){
+    for(const child of buttons.children){
+        if(child.id != "ac" && child.id != "c"){
+            child.onclick = () => add_number(Number(child.innerHTML));
+        }
+    }
+
+    const clear_button = document.querySelector("#ac");
+    clear_button.onclick = clear_all;
+}
+
+
+window.onload = () => {
+    init_buttons();
+}

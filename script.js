eeaf34cb4128e6d0c5167ece97356cbeb8bb13d1
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-12-22 14:14:26 +0000

    Add unary operation functionality

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 const calculator = document.querySelector(".calculator");
 const buttons = document.querySelector(".buttons");
-const calculator_output = document.querySelector(".calculator_output")
+const calculator_output = document.querySelector(".calculator_output");
+const prev_output = document.querySelector(".prev_output");
 let output_buffer = "";
+let prev_buffer = "";
 let operator = "";
 let num1 = null;
 let num2 = null;
 
 function add_number(number){
-    if(output_buffer === ""){
-        output_buffer = number;
-    }
-    else{
-        output_buffer = output_buffer * 10 + number;
-    }
+    if(!(number === "." && output_buffer.includes('.')))
+        output_buffer += number;
     calculator_output.innerHTML = output_buffer;
 }
 
 function clear_all(){
-    if(output_buffer != ""){
-        output_buffer = "";
-    }
-    calculator_output.innerHTML = output_buffer;
-}
+    output_buffer = "";
+    num1 = null;
+    num2 = null;
+    operator = "";
+    prev_buffer = "";
 
-function clear_last(){
-    if(output_buffer === "" || output_buffer < 10){
-        output_buffer = "";
-    }
-    else{
-        output_buffer = (output_buffer - (output_buffer % 10)) / 10
-    }
     calculator_output.innerHTML = output_buffer;
+    prev_output.innerHTML = prev_buffer;
 }
 
 function init_buttons(){
     for(const child of buttons.children){
         if(child.classList.contains("number")){
-            child.onclick = () => add_number(Number(child.innerHTML));
+            child.onclick = () => add_number(child.innerHTML);
         }
         if(child.classList.contains("operator")){
             child.onclick = () => set_operator(child.innerHTML);
@@ -48,6 +40,9 @@ function init_buttons(){
                     operations(operator);
             }
         }
+        if(child.classList.contains("unary")){
+            child.onclick = () => unary_operations(child.innerHTML);
+        }
     }
 
     const clear_button = document.querySelector("#ac");
@@ -55,38 +50,63 @@ function init_buttons(){
 }
 
 function equal(){
+
     if(num1 === null){
         num1 = output_buffer;
+        prev_buffer = num1 + " " + operator;
+    }
+    else if(num2 === null){
+        num2 = output_buffer;
+        prev_buffer = num1 + " " + operator + " " + num2;
     }
     else{
-        num2 = output_buffer;
+        num1 = output_buffer;
+        prev_buffer = num1 + " " + operator + " " + num2;
     }
+    prev_output.innerHTML = prev_buffer;
     output_buffer = "";
     calculator_output.innerHTML = output_buffer;
 }
 
 function set_operator(inner_operator){
+    if(num1 != null & num2 != null){
+        num2 = null;
+    }
     operator = inner_operator;
     equal();
 }
 
 function operations(operator){
     if(operator === '+'){
-        output_buffer = num1 + num2;
+        output_buffer = (Number(num1) + Number(num2)).toString();
     }
     if(operator === '-'){
-        output_buffer = num1 - num2;
+        output_buffer = (Number(num1) - Number(num2)).toString();
     }
     if(operator === '/'){
-        output_buffer = num1 / num2;
+        output_buffer = (Number(num1) / Number(num2)).toString();
     }
     if(operator === '*'){
-        output_buffer = num1 * num2;
+        output_buffer = (Number(num1) * Number(num2)).toString();
     }
     operator = "";
     calculator_output.innerHTML = output_buffer;
 }
 
+function unary_operations(operator){
+    if(operator === '%'){
+        output_buffer = output_buffer / 100;
+    }
+    if(operator === "+/-"){
+        output_buffer = -output_buffer;
+    }
+    calculator_output.innerHTML = output_buffer;
+}
+
+function show_error(){
+    clear_all();
+    output_buffer = "ERR";
+}
 
 window.onload = () => {
     init_buttons();
